6462cb8f6736423efdd7d232a29f7925e5f25cd4
mohammadrashid <mohammadrashid6732@gmail.com>
2024-12-19 13:09:09 +0330

    write code for show product and details

---
+++ models/products.js
@@ -0,0 +1,40 @@
+class Products {
+  constructor(parent, products) {
+    this.parent = parent;
+    this.products = products;
+  }
+
+  showProducts() {
+    this.products.forEach((product) => this.createCard(product));
+  }
+  createCard(data) {
+    const cardEle = document.createElement("div");
+    const imgEle = this.productImg(data);
+    const productEle = this.productInfo(data);
+
+    cardEle.innerHTML = imgEle;
+    cardEle.innerHTML += productEle;
+    this.parent.appendChild(cardEle);
+  }
+  productImg(data) {
+    const { image, alt } = data;
+    const imgJsx = `<img src=${image} alt=${alt} />`;
+
+    return imgJsx;
+  }
+  productInfo(data) {
+    const { id, name, price } = data;
+    const productJsx = ` 
+      <div>
+        <h3>${name}</h3>
+        <div>
+          <span>${price}</span>
+          <button>+</button>
+        </div>
+      </div>
+    `;
+    return productJsx;
+  }
+}
+
+export default Products;
